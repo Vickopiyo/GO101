9ed8e5a4fd5d78d00fbd5b9536dc894f8eddcb97
Vickopiyo <opiyookoth2016@gmail.com>
2023-01-16 14:11:13 +0300

    packages and Variables

---
+++ Intro/variable/variable.go
@@ -0,0 +1,25 @@
+
+package main 
+
+import "fmt"
+
+func main()  {
+	
+// declaring variable using var 
+// All imports and variables declared must be used  in GO programms  
+    // long way to declare variables 
+   var student1 string 
+     student1 = "Vick"
+	 var student string 
+	 student = "Opiyo"   
+// Shortest way of declaring variable declaring and initialization  
+// integer--int while dec numbers are float64 
+   age := 26           
+// equivalent to console.log of JS 
+   fmt.Println(student1, student, age)
+
+
+
+
+
+}    
