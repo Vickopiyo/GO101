0edfe3fa44b466682dae73d053624914a4cb2714
Vickopiyo <opiyookoth2016@gmail.com>
2023-01-16 15:49:20 +0300

    Division and modulus

--- Intro/Arithmetics/arithmetics.go
+++ Intro/Arithmetics/arithmetics.go
@@ -15,6 +15,19 @@ func main()  {
    fmt.Println("Results Two is: ", result)    
 
    result = a * b 
-   fmt.Println("Result Three is:", result)
+   fmt.Println("Result Three is:", result)    
 
-}
+   result = b / a 
+    // Even though accurate rssults was 1.5..it prints out  1 beacuse result was initialized as int  not float64
+   fmt.Println("Results four is :", result )       
+   
+//    TO get results i  float64 always  declare var to be float64 and depednded varibale to be in float64 format too 
+
+var div float64 = 6.0 / 4.0   
+    fmt.Println("Division ",div)   
+
+	// Modulus--only applicable to to integers 
+	div =  6 % 4     
+	fmt.Println("Modulus :",div)
+       
+}        
