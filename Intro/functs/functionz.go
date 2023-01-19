d04cad657d1e0cbaa86fcc94ff6de3eff7572956
Vickopiyo <opiyookoth2016@gmail.com>
2023-01-19 21:14:31 +0300

    Recursion functs

--- Intro/functs/functionz.go
+++ Intro/functs/functionz.go
@@ -1,30 +1,63 @@
-package main  
-import "fmt"   
+package main
 
-// type of parameter must be declared when passing it  
+import "fmt"
+// type of parameter must be declared when passing it
 
-func greeting(name string)  {
-	fmt.Printf("Hello  %s ,Functions!!",  name)      
-}              
-            
-// Types of parameters and return MUST always be declared...Only applicable to functions with return statement    
-// if more than two params of same type..declaration of type is done after the last params as seen below 
-// declaraion type of return is done before the body of function     
+func greeting(name string) {
+	fmt.Printf("Hello  %s ,Functions!!", name)
+}
 
-func sum (a, b int) int {  
+// Types of parameters and return MUST always be declared...Only applicable to functions with return statement
+// if more than two params of same type..declaration of type is done after the last params as seen below
+// declaraion type of return is done before the body of function
 
-       return a + b 
-}       
- 
-func main()  {   
-	// How to call functions in GOLang
-   greeting("VICK")	  
-   r := sum( 43 , 45)   
-   fmt.Println("Sum is :",r)                
-}          
+func sum(a, b int) int {
+	return a + b
+}
 
+// ELIPSIS   passed before type in the function as params means it can take any number of args of int type(variadic Function)
+// otherwise it returns empty slice []
+func sumVariadicFunc(numbers ...int) int {
 
-
-
-
+	var total int
+	for _, number := range numbers {
+		total += number
+	}
    
+	fmt.Println(total)  
+   // If rerurns goes above it return missing return.Return should always be the last line in a GO func 
+	return total
+}
+   
+// Returning more than one datatype like string and int therefore in return the two must be returned    
+// ELIPSIS passed in a function always placed at the end of the params
+ func varidaicFunc(number string, numeros ...int)  (string, int){   
+    
+   // OUR RETURNS VALUES...SPRINTF used when value(string) is passed to func 
+   var sum int   
+   message :=fmt.Sprintf("Hello %s , here is our Sum",number)       
+
+   for _, num := range numeros { 
+      sum +=num
+   }         
+   fmt.Println(sum)  
+
+   return message, sum        
+                            
+ }
+
+func main() {
+	// How to call functions in GOLang
+	// greeting("VICK")
+	// r := sum( 43 , 45)
+	// fmt.Println("Sum is :",r)
+	sumVariadicFunc(3, 3, 3, 3, 3, 5)     
+
+//   create two new variables even though names are similar but they are block scoped.
+//   one variable takes the value of message and the other sum bcz fun returns two objects 
+
+    info,sum :=varidaicFunc("Vick",5,6,7,7,8)    
+
+    fmt.Println(info, sum)
+                     
+}
---
+++ Intro/functs/recursion.go
@@ -0,0 +1,30 @@
+package main  
+         
+import "fmt"  
+
+
+
+  func factorial(n int)  int{  
+    //  TAIL RECURSION--a func calt itself calculates value and sends it down the hierachy
+	// KEY BENEFIT--- always faster than normal recursion   
+    //   BASE condition...if met recursion stops  
+	   if n == 0 {
+		return 1
+	   }  
+    //  outputs 5,4,3,2,1 ...stops when it reaches 0        
+	   fmt.Println(n)                     
+	//first instance it gets 5 then it call then get 4 then 3 then 2 then 1  ()  
+    // After base condition is met then last output assumes value of n and     
+	// outputs 1,2,6,24,120  
+	 //  tail recursive call     
+    f := n * factorial(n-1)                                   
+    fmt.Println(f)                                   
+    return f                                     
+	           
+  }
+
+func main()  {   
+    
+  factorial(5)    
+	
+}                 
